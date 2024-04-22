2148d237934e9f84c8ffb073fbb1995c6180113b
David-san <optistech@Mac-Mini.local>
2024-04-22 14:09:03 +0700

    deploy new version

--- lib/const/const.dart
+++ lib/const/const.dart
@@ -2,7 +2,7 @@ class ConstValue {
   static String success = "success";
   static String japanese = "japanese";
   static String foreigner = "foreigner";
-  static String appVersion = "1.0.0+49";
+  static String appVersion = "1.0.0+50";
   static String defaultBgImage =
       "https://firebasestorage.googleapis.com/v0/b/air-job.appspot.com/o/appstore.png?alt=media&token=62053706-8a73-43a8-bbf5-5dc2849fec40";
   static String defaultImage =
